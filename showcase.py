bc6f6c3567478f779b3ccd505828fd1df5a0ee1b
Anung B Ariwibowo <barliant@gmail.com>
2024-12-12 15:42:08 +0700

    Initial commit showcase.py, with projects from Gasal 2024/2025.

---
+++ showcase.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+st.title("Proyek Kelompok Praktikum Analisa Big Data Ekonomika (ELA6105)")
+
+st.write("# Semester Gasal 2024/2025")
+st.markdown('''
+- [Kelompok Bunga Abadi](https://tugaskelompok.streamlit.app/)
+- [Kelompok PasmingBased](https://pasmingbased-part1.streamlit.app/)
+- [Kelompok Mercon Jontor](https://mercon-jontor.streamlit.app/)
+- [Kelompok GoFooodBoleeeh](https://gofooodboleeeh.streamlit.app/) (Yes, with triple `o` and triple `e`)
+- [Kelompok TimPemburu](https://timpemburu.streamlit.app/)
+            - [Kelompok Dududada]
+''')
+
