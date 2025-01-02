89119387467c799401668206328494776dd56966
Anung B Ariwibowo <barliant@gmail.com>
2025-01-02 14:24:40 +0700

    Add backlink to domain trisakti.ac.id.

--- showcase.py
+++ showcase.py
@@ -2,8 +2,30 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
+st.set_page_config(layout="wide")
+
 st.title("Proyek Kelompok Praktikum Analisa Big Data Ekonomika (ELA6105)")
 
+st.markdown('''
+            Berikut ini adalah arsip Proyek Kelompok mata kuliah
+            Praktikum Analisa Big Data Ekonomika (ELA6105) yang
+            diselenggarakan oleh Program Studi Ekonomi Pembangunan,
+            Fakultas Ekonomi dan Bisnis, Universitas Trisakti.
+            ''')
+
+st.markdown('''
+            Kuliah ini mulai dilaksanakan melalui kegiatan
+            [_Training of Trainer_](https://kuikk.trisakti.ac.id/news/training-of-trainers-tot-praktikum-analisis-big-data-ekonomi-program-studi-s1-ekonomi-pembangunan-feb-usakti/)
+            sebagai bagian dari pelaksanaan mata kuliah Praktikum
+            dalam Kurikulum Program Studi Ekonomi Pembangunan. Sebagai
+            dosen koordinator ditunjuk ibu
+            [Sri Yani Kusumastuti, SE, MSI](https://ep.feb.trisakti.ac.id/tentang/dosen-prodi-ep/).
+
+            Kuliah ini dilaksanakan secara _team teaching_ dengan
+            dosen pengajar dari Program Studi Sarjana Sistem
+            Informasi, Fakultas Teknologi Industri, bapak [Anung B
+            Ariwibowo, SKom, MKom](https://sistem-informasi.fti.trisakti.ac.id/profil/dosen/)
+            ''')
 st.write("# Semester Gasal 2024/2025")
 st.markdown('''
 - [Kelompok Bunga Abadi](https://tugaskelompok.streamlit.app/)
