cd1d3273419c099ac4d3a690c09736fac1e86d01
Anung B Ariwibowo <barliant@gmail.com>
2025-01-02 14:41:09 +0700

    Fixing minor typo.

--- showcase.py
+++ showcase.py
@@ -11,27 +11,27 @@ st.markdown('''
             Praktikum Analisa Big Data Ekonomika (ELA6105) yang
             diselenggarakan oleh Program Studi Ekonomi Pembangunan,
             Fakultas Ekonomi dan Bisnis, Universitas Trisakti.
-            ''')
 
-st.markdown('''
             Kuliah ini mulai dilaksanakan melalui kegiatan
             [_Training of Trainer_](https://kuikk.trisakti.ac.id/news/training-of-trainers-tot-praktikum-analisis-big-data-ekonomi-program-studi-s1-ekonomi-pembangunan-feb-usakti/)
             sebagai bagian dari pelaksanaan mata kuliah Praktikum
             dalam Kurikulum Program Studi Ekonomi Pembangunan. Sebagai
             dosen koordinator ditunjuk ibu
             [Sri Yani Kusumastuti, SE, MSI](https://ep.feb.trisakti.ac.id/tentang/dosen-prodi-ep/).
-
             Kuliah ini dilaksanakan secara _team teaching_ dengan
             dosen pengajar dari Program Studi Sarjana Sistem
             Informasi, Fakultas Teknologi Industri, bapak [Anung B
             Ariwibowo, SKom, MKom](https://sistem-informasi.fti.trisakti.ac.id/profil/dosen/)
             ''')
+
 st.write("# Semester Gasal 2024/2025")
 st.markdown('''
 - [Kelompok Bunga Abadi](https://tugaskelompok.streamlit.app/)
 - [Kelompok PasmingBased](https://pasmingbased-part1.streamlit.app/)
 - [Kelompok Mercon Jontor](https://mercon-jontor.streamlit.app/)
-- [Kelompok GoFooodBoleeeh](https://gofooodboleeeh.streamlit.app/) (Yes, with triple `o` and triple `e`)
+- [Kelompok GoFooodBoleeeh](https://gofooodboleeeh.streamlit.app/)
+            (Yes, with triple `o` inf _Foood_ and triple `e` in
+            _Boleeeh_)
 - [Kelompok Dududada](https://almanacproject.streamlit.app/)
 - [Kelompok TimPemburu](https://timpemburu.streamlit.app/)
 - [Kelompok MinyakHitam](https://minyakhitam.streamlit.app/)
